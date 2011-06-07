20d041f964c23b3d9454f23905711c9b0bf7a2d8
unknown <Jonas Bang@.(none)>
2011-06-07 20:03:37 +0200

    Add page title to all pages except the front page.

--- index.php
+++ index.php
@@ -9,12 +9,14 @@ get_header();
 <div id="page-wrap">
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-    <?php if (!is_page()) { ?>
+    <?php if (!is_page()) : ?>
 	<h3 class="storytitle"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 	<p class="comment-count"><?php comments_popup_link(__('<span class="count">0</span> kommentarer'), __('<span class="count">1</span> kommentar'), __('<span class="count">%</span> kommentarer')); ?></p>
 	<p class="meta">Skrevet <?php the_time('j. F Y') ?> af <?php the_author() ?>  i <?php the_category(' |') ?><br><?php edit_post_link( __( 'Rediger', 'jbwindtwo' ), '<span class="edit-link">', '</span>' ); ?></p>
 	<p class="the-tags"><?php the_tags(__(''), ', ', ''); ?></p>
-    <?php } ?>
+    <?php elseif (!is_front_page()) : ?>
+	<h3 class="pagetitle"><?php the_title(); ?></h3>
+    <?php endif; ?>
 	<div class="storycontent">
 		<?php the_content(__('(more...)')); ?>
 		<div class="clr"></div>
@@ -27,9 +29,9 @@ get_header();
 
 <?php if (!is_page()) { comments_template(); } ?>
 
-<?php if (is_page()) { ?>
+<?php if (is_page()) : ?>
     <?php edit_post_link( __( 'Rediger', 'jbwindtwo' ), '<span class="edit-link">', '</span>' ); ?>
-<?php } ?>
+<?php endif; ?>
 
 <?php endwhile; else: ?>
 <p><?php _e('Beklager, ingen indlæg matcher dine kriterier.'); ?></p>
