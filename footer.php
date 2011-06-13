8bd2bc9218b037f66b39b30de858f46a717e20a3
unknown <Jonas Bang@.(none)>
2011-06-13 17:03:23 +0200

    Change credits text.

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 	<div class="footerlinks">
 		<div class="allrights">&copy; <?php bloginfo('name');?> - All rights reserved</div>
 		<div class="credit">
-Design by: <a href="http://www.theforge.co.za" title="Cape Town Web Designers">The Forge Web Creations</a> | Customized by: <a href="http://www.uhuru.dk" title="Uhuru Web Design">Uhuru Web Design</a></div>
+Design by: <a href="http://www.theforge.co.za" title="Cape Town Web Designers">The Forge Web Creations</a> | Theme by: <a href="http://www.uhuru.dk" title="Uhuru Web Design">Uhuru Web Design</a></div>
 	</div>
 
 </div>
