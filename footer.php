cf68696fcfc1e4f5c4911d744f123a238c87e7db
Jonas Bang <email@jonasbang.dk>
2012-11-14 17:10:55 +0100

    Change footer credits

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 	<div class="footerlinks">
 		<div class="allrights">&copy; <?php bloginfo('name');?> - All rights reserved</div>
 		<div class="credit">
-Theme by: <a href="http://www.uhuru.dk" title="Uhuru Web Solutions">Uhuru Web Solutions</a> | Design by: <a href="http://www.theforge.co.za" title="Cape Town Web Designers">The Forge Web Creations</a></div>
+Theme design by: <a href="http://www.theforge.co.za" title="Cape Town Web Designers">The Forge Web Creations</a></div>
 	</div>
 
 </div>
